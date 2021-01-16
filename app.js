7abb0f9cc255d4cdaa8d00ea4bba6ffd522dd0b2
AwPouk <aw.pouk@gmail.com>
2021-01-16 14:35:01 +0100

    adding chech mark and checj trash

--- app.js
+++ app.js
@@ -19,4 +19,14 @@ function addTodo(event){
     newTodo.innerText = 'hey';
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo);
+    //CHECK MARK BUTTON
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class-"fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //CHECK trash BUTTON
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class-"fas fa-trash"></i>';
+    trashButton.classList.add("complete-btn");
+    todoDiv.appendChild(trashButton);
 }
